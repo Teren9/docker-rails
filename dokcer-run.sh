ea155173bb6eb00e6f3c759a3122e5d9e449ebd5
Almog Friedman <teren9@gmail.com>
2015-08-30 13:30:30 +0300

    sh file for docker v3

--- dokcer-run.sh
+++ dokcer-run.sh
@@ -2,7 +2,7 @@
 docker stop app
 docker rm app
 docker build -t docker-rails .
-docker run -v "/home/almog/Desktop/docker test/docker-rails":/usr/src/app --name app -d -p 80:3000 docker-rails
+docker run -v "$PWD":/usr/src/app --name app -d -p 80:3000 docker-rails
 
 docker exec -i -t app bash
 
